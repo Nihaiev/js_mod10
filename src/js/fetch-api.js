9c065187752f8ed7001982ea7406c4996a5c8aa4
Иван Нигаев <Nihaiev@it.group>
2022-08-28 20:48:40 +0300

    added src

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,16 +1,35 @@
- /**
-  * Http запити в барузері
-  */
+/**
+ * Http запити в барузері
+ */
 
- inport '../css/common.css'
+import '../css/common.css';
+import pokemoncardTpl from '../template/pokemon-card.hbs';
+import API from './api-service'; //робота з бекендом
+import getRefs from './get-refs'; //отримання рефів
 
-fetch('https://pokeapi.co/api/v2/pokemon/2').then(response => {
+const refs = getRefs
 
-    return response.json();
-})
-    .then(pokemon => {
-     console.log(pokemon);
-    })
-    .catch(error => {
-     console.log(error);
- })
+// робота з інтерфейсом 
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  API.fetchPokemon(searchQuery)
+    .then(renderpokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
+
+function renderpokemonCard(pokemon) {
+  const markup = pokemoncardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert(' Упс, щось пішло не так');
+}
